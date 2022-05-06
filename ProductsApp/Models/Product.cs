b4257eb4287b7f4fdf02a1ad8e57fe296ed7a275
JAnderson217 <24385176+JAnderson217@users.noreply.github.com>
2022-05-06 15:24:34 +0100

    MVC lab work example

---
+++ ProductsApp/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductsApp.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string productName { get; set; }
+        public decimal price { get; set; }
+        public string description { get; set; } 
+    }
+}
